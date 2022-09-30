d3cfdf0d1162c117cf7536b3be743d979d2dcfca
Soro <soro@Soros-MacBook-Pro.local>
2022-09-29 22:38:00 -0400

    Added plain pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
           backgroundColor: '#121212', // Used in manifest.json
           themeColor: '#121212', // Used in manifest.json
           display: 'minimal-ui', // Used in manifest.json
-          crossOrigin: `use-credentials`,
         },
         contentDirectory: './content',
         blogSettings: {
@@ -26,6 +25,24 @@ module.exports = {
         // }
       },
     },
-    'gatsby-plugin-offline'
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "WeAreMahsaAmini",
+          short_name: "WeAreMahsaAmini",
+          start_url: "/",
+          background_color: "#121212",
+          theme_color: "#121212",
+          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+          display: "standalone",
+          icon: './content/images/favicon.png', // This path is relative to the root of the site.
+          // An optional attribute which provides support for CORS check.
+          // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+          // Any invalid keyword or empty string defaults to `anonymous`
+          crossOrigin: `use-credentials`,
+        },
+      },
+    'gatsby-plugin-offline',
   ],
 };
