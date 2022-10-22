f41f2cb66d440810250cc80c42c735a86443fafa
Siavash <siavash@tribe.so>
2022-10-22 19:48:43 -0400

    Simplified feat count page

--- src/pages/death-count.jsx
+++ src/pages/death-count.jsx
@@ -3,8 +3,13 @@ import { Animation, Page, Section, Seo } from 'gatsby-theme-portfolio-minimal';
 import CountUp from 'react-countup';
 
 // eslint-disable-next-line react/prop-types
-export default function QRCode({ serverData }) {
-  const { total_executions, juvenile_executions, women_executions } = serverData || {};
+export default function DeatCount() {
+  const data = {
+    total_executions: 435,
+    juvenile_executions: 2,
+    women_executions: 12,
+  };
+
   return (
     <>
       <Seo title="Gallery" useTitleTemplate noIndex />
@@ -13,13 +18,13 @@ export default function QRCode({ serverData }) {
           <div style={{ textAlign: 'center' }}>
             <Section anchor="deathcount" heading="2022 Protests Death Count">
               <p className="cta-button">
-                Total executions: <CountUp duration="1" end={total_executions} />
+                Total executions: <CountUp duration="1" end={data.total_executions} />
               </p>
               <p className="cta-button">
-                Juvenile executions: <CountUp duration="1" end={juvenile_executions} />
+                Juvenile executions: <CountUp duration="1" end={data.juvenile_executions} />
               </p>
               <p className="cta-button">
-                Women executions: <CountUp duration="1" end={women_executions} />
+                Women executions: <CountUp duration="1" end={data.women_executions} />
               </p>
             </Section>
           </div>
@@ -27,35 +32,4 @@ export default function QRCode({ serverData }) {
       </Page>
     </>
   );
-}
-
-export async function getServerData() {
-  try {
-    // https://iranhr.net/en/ is protected by cloudflare, so bots cannot access
-    // I am working on alternatives. In the mean time, let's hard code the values
-    // I manually pulled from https://iranhr.net/en/.
-    // const res = await fetch(`https://iranhr.net/en/`);
-
-    // if (!res.ok) {
-    //   throw new Error(`Response failed`);
-    // }
-
-    // const retData = await res.json();
-
-    const retData = {
-      total_executions: 435,
-      juvenile_executions: 2,
-      women_executions: 12,
-    };
-
-    return {
-      props: await retData,
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      headers: {},
-      props: {},
-    };
-  }
 }
